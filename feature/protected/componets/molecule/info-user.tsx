028f366681fc7a58838bad7979c211734d6e6ac5
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-05-22 11:43:29 -0500

    fix: correct email domain format in InfoUser component

--- feature/protected/componets/molecule/info-user.tsx
+++ feature/protected/componets/molecule/info-user.tsx
@@ -16,9 +16,7 @@ export default function InfoUser({ avatarUrl, name }: NavSectionsProps) {
       />
       <div className="flex flex-col">
         <Label className="text-primary-foreground text-xs">{name}</Label>
-        <span className="text-primary-foreground text-xs">
-          Hello@{name}.com
-        </span>
+        <span className="text-primary-foreground text-xs">Hello@{name}.co</span>
       </div>
       <ChevronDown />
     </section>
