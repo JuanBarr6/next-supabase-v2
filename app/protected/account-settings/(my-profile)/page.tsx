8d44eb12e852e85b0f3f20d0c9b264a88f682d1d
JuanBarr6 <juanm.barrerao06@gmail.com>
2025-06-26 10:21:06 -0500

    feat: overflow-y example

--- app/protected/account-settings/(my-profile)/page.tsx
+++ app/protected/account-settings/(my-profile)/page.tsx
@@ -1,3 +1,14 @@
 export default function MyProfile() {
-  return <div className="text-primary-foreground">esto es my MyProfile</div>;
+  return (
+    <div className="space-y-1">
+      {[...Array(12)].map((_, i) => (
+        <div
+          key={i}
+          className="h-[200px] w-[200px] bg-[#34367f] text-primary-foreground"
+        >
+          {i}
+        </div>
+      ))}
+    </div>
+  );
 }
--- app/protected/account-settings/layout.tsx
+++ app/protected/account-settings/layout.tsx
@@ -8,7 +8,7 @@ export default function AccountSettings({
   return (
     <div className="flex bg-background h-full w-full flex-col gap-5">
       <Label className="text-2xl">Account Settings</Label>
-      <div className="flex  h-full p-4 rounded-2xl flex-row w-full bg-primary">
+      <div className="flex h-full p-4 rounded-2xl flex-row w-full bg-primary overflow-y-auto scrollbar-none scrollbar">
         {children}
       </div>
     </div>
