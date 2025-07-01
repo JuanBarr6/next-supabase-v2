0676a7d0e8e613191e7cdd603d711e4c73ed82ab
juan-barr <bath.barrera@gmail.com>
2025-07-01 10:42:33 -0500

    feat: more project structure

--- app/protected/account-settings/layout.tsx
+++ app/protected/account-settings/layout.tsx
@@ -1,4 +1,5 @@
 import { Label } from "@/components/ui/label";
+import NavAccountSettings from "@/feature/protected/sub-features/account-settings/components/nav-account-settings";
 
 export default function AccountSettings({
   children,
@@ -8,6 +9,7 @@ export default function AccountSettings({
   return (
     <div className="flex bg-background h-full w-full flex-col gap-5">
       <Label className="text-2xl">Account Settings</Label>
+      <NavAccountSettings />
       <div className="flex h-full p-4 rounded-2xl flex-row w-full bg-primary overflow-y-auto scrollbar-none scrollbar">
         {children}
       </div>
