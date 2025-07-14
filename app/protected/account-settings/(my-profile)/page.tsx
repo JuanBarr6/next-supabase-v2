318bf525d15c8f71bd9c4fd5e7174a2351ac0b7d
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-07-14 11:08:37 -0500

    feat: add message handling in My Profile and display in Security page; implement input and button for user interaction

--- app/protected/account-settings/(my-profile)/page.tsx
+++ app/protected/account-settings/(my-profile)/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import { UserProfile } from "@/feature/protected/componets/sub-features/account-seetings/sub-features/my-profile/components/user-profile";
 import { PersonalInfo } from "@/feature/protected/componets/sub-features/account-seetings/sub-features/my-profile/components/personal-info";
 import { AddressInfo } from "@/feature/protected/componets/sub-features/account-seetings/sub-features/my-profile/components/address-info";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function MyProfile() {
+  const router = useRouter();
+  const [mensaje, setMensaje] = useState("");
+
+  const handleTesRender = () => {
+    const encoded = encodeURIComponent(mensaje);
+    router.push(`/protected/account-settings/security?mensaje=${encoded}`);
+  };
   return (
     <div className="space-y-1 bg-white w-full h-full">
       <h1 className="font-bold">My Profile</h1>
@@ -9,6 +21,22 @@ export default function MyProfile() {
         <UserProfile />
         <PersonalInfo />
         <AddressInfo />
+      </div>
+      <div className="w-full flex flex-col bg-white text-black  p-4">
+        <UserProfile />
+        <PersonalInfo />
+        <AddressInfo />
+      </div>
+      <div className="flex flex-col w-[500px] bg-white text-black gap-4 p-4">
+        <input
+          type="text"
+          placeholder="Escribe un mensaje"
+          className="border p-2 rounded text-black"
+          value={mensaje}
+          onChange={(e) => setMensaje(e.target.value)}
+        />
+
+        <Button onClick={handleTesRender}>Enviar prueba</Button>
       </div>
     </div>
   );
--- app/protected/account-settings/security/page.tsx
+++ app/protected/account-settings/security/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function Security() {
-  return <div className="text-black">esto es Security</div>;
+  const searchParams = useSearchParams();
+  const mensaje = searchParams.get("mensaje");
+
+  return (
+    <div className="text-black p-4">
+      <h2 className="font-bold text-lg">Esto es Security</h2>
+      <div className="bg-white p-4 rounded shadow">
+        <p className="text-black">
+          {mensaje
+            ? `Mensaje recibido: ${mensaje}`
+            : "No se recibió ningún mensaje"}
+        </p>
+      </div>
+    </div>
+  );
 }
