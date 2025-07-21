660df502b16a0c4c5214799bb646ac1bfb6ebf69
JSebastian042 <juansebastianjuradotorres@gmail.com>
2025-07-21 01:39:11 -0500

    Copié mis cambios desde la carpeta local

---
+++ feature/auth-pages/sign-in/componets/molecule/nav-sections.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+interface NavSectionsProps {
+  Icon: React.ElementType;
+  title: string;
+}
+
+export default function NavSection({ Icon, title }: NavSectionsProps) {
+  return (
+    <Link
+      href={`/protected/account-settings`}
+      className="flex items-center gap-2 px-4 py-1 rounded-sm w-full hover:bg-primary-foreground/20 transition-colors duration-200 ease-in-out cursor-pointer"
+    >
+      <Icon className="text-primary-foreground" />
+      <Label className="text-primary-foreground">{title}</Label>
+    </Link>
+  );
+}
---
+++ feature/auth-pages/sign-in/componets/molecule/themetoggle.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") as
+      | "light"
+      | "dark"
+      | null;
+
+    if (storedTheme) {
+      setTheme(storedTheme);
+      if (storedTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    } else {
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      const defaultTheme = prefersDark ? "dark" : "light";
+      setTheme(defaultTheme);
+      if (prefersDark) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    if (newTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-full hover:bg-muted transition"
+      aria-label="Toggle theme"
+    >
+      {theme === "dark" ? (
+        <Sun className="w-5 h-5" />
+      ) : (
+        <Moon className="w-5 h-5" />
+      )}
+    </button>
+  );
+}
