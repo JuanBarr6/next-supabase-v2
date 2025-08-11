225af5c57925223e76d086d747e1029bb424a2d3
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-08-11 10:22:40 -0500

    fix(editable-cell): add date picker support and update task save logic

--- feature/protected/componets/sub-features/account-seetings/sub-features/to-do/components/edit-to-do.tsx
+++ feature/protected/componets/sub-features/account-seetings/sub-features/to-do/components/edit-to-do.tsx
@@ -1,24 +1,30 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { Calendar22 } from "../../../components/calendar";
 
 interface EditableCellProps {
   initialTask: string;
   initialStatus: string;
+  initialDate: string;
   id_task: number;
-  updateTask: (id: number, task: string, status: string) => void;
+  updateTask: (id: number, task: string, status: string, date: string) => void;
   cancelEditing: () => void;
 }
 
 export function EditableCell({
   initialTask,
   initialStatus,
+  initialDate,
   id_task,
   updateTask,
   cancelEditing,
 }: EditableCellProps) {
   const [editedTaskText, setEditedTaskText] = useState(initialTask);
   const [editedTaskStatus, setEditedTaskStatus] = useState(initialStatus);
+  const [editedTaskDate, setEditedTaskDate] = useState<Date | undefined>(
+    initialDate ? new Date(initialDate) : undefined
+  );
 
   useEffect(() => {
     setEditedTaskText(initialTask);
@@ -26,14 +32,15 @@ export function EditableCell({
   }, [initialTask, initialStatus]);
 
   const handleSave = () => {
-    updateTask(id_task, editedTaskText, editedTaskStatus);
+    const formattedDate = editedTaskDate?.toISOString().split("T")[0] || "";
+    updateTask(id_task, editedTaskText, editedTaskStatus, formattedDate);
   };
 
   return (
-    <div className="flex flex-row gap-2">
+    <div className="flex flex-row w-full gap-2">
       <input
         type="text"
-        className="border p-2 rounded text-black"
+        className="border p-2 rounded text-black w-auto"
         value={editedTaskText}
         onChange={(e) => setEditedTaskText(e.target.value)}
       />
@@ -45,6 +52,10 @@ export function EditableCell({
         <option value="pendiente">Pendiente</option>
         <option value="completada">Completada</option>
       </select>
+      <Calendar22
+        placeholderText="Fecha a realizar tarea"
+        onDateChange={(date) => setEditedTaskDate(date)}
+      />
       <div className="flex gap-2 mt-2">
         <Button onClick={handleSave}>Guardar</Button>
         <Button variant="ghost" onClick={cancelEditing}>
