3cc0fdb7f1c4603018b5b33947a4be0527d14193
sebas.Opi.Tech <sebastian.alvarezo@opitech.com.co>
2025-08-11 10:24:06 -0500

    refactor(todo): modularize components and add date filtering

--- feature/protected/componets/sub-features/account-seetings/sub-features/to-do/components/list-to-do.tsx
+++ feature/protected/componets/sub-features/account-seetings/sub-features/to-do/components/list-to-do.tsx
@@ -1,29 +1,29 @@
 "use client";
 import React, { useState, useEffect, useMemo } from "react";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import { Session } from "@supabase/supabase-js";
 import { createClient } from "@/utils/supabase/client";
 import { EditableCell } from "./edit-to-do";
+import { CheckCircle, Clock, Pencil, Trash } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Loading from "../../../components/loading";
+import { validationToDo } from "../validations/validation-create-to-do";
+import { CreateToDo } from "./create-to-do";
+import { FilterToDo } from "./filter-to-do";
+import { TableToDo } from "./table-to-do";
 
 interface Task {
   id_task: number;
   task: string;
   status: string;
   user_id: string;
+  execution_date: string;
 }
 
 export function ListToDo() {
@@ -31,11 +31,20 @@ export function ListToDo() {
   const [data, setData] = useState<Task[]>([]);
   const [task, setTask] = useState("");
   const [status, setStatus] = useState("");
+  const [executionDate, setExecutionDate] = useState<Date | undefined>(
+    undefined
+  );
   const supabase2 = createClient();
-
   const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
   const [editedTaskText, setEditedTaskText] = useState("");
   const [editedTaskStatus, setEditedTaskStatus] = useState("");
+  const [filterDate, setFilterDate] = useState<Date | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState<{
+    description?: string;
+    status?: string;
+    date?: string;
+  }>({});
 
   const fetchTasks = async (userId?: string) => {
     const { data, error } = await supabase2
@@ -52,6 +61,26 @@ export function ListToDo() {
   };
 
   const handleTesRender = async () => {
+    const result = validationToDo.safeParse({
+      description: task,
+      status: status,
+      date: executionDate?.toISOString().split("T")[0] ?? "",
+    });
+
+    if (!result.success) {
+      const fieldErrors: any = result.error.format();
+
+      setErrors({
+        description: fieldErrors.description?._errors?.[0],
+        status: fieldErrors.status?._errors?.[0],
+        date: fieldErrors.date?._errors?.[0],
+      });
+
+      return;
+    }
+
+    setErrors({});
+
     if (!task.trim() || !status.trim() || !session?.user.id) {
       console.log("Faltan datos o sesión");
       return;
@@ -61,6 +90,7 @@ export function ListToDo() {
       task,
       status,
       user_id: session.user.id,
+      execution_date: executionDate?.toISOString().split("T")[0],
     });
 
     if (error) {
@@ -68,6 +98,7 @@ export function ListToDo() {
     } else {
       setTask("");
       setStatus("");
+      setExecutionDate(undefined);
       fetchTasks(session.user.id);
     }
   };
@@ -92,6 +123,7 @@ export function ListToDo() {
     if (error) {
       console.error("Error al eliminar tarea:", error.message);
     } else {
+      console.log("Tarea eliminada correctamente");
       if (session?.user.id) {
         fetchTasks(session.user.id);
       }
@@ -99,6 +131,7 @@ export function ListToDo() {
   };
 
   const updateTask = async (id: number, newTask: string, newStatus: string) => {
+    setIsLoading(true);
     const { error } = await supabase2
       .from("to-do")
       .update({ task: newTask, status: newStatus })
@@ -110,29 +143,55 @@ export function ListToDo() {
       fetchTasks(session.user.id);
       setEditingTaskId(null);
     }
+    setIsLoading(false);
   };
+
+  const filteredData = useMemo(() => {
+    if (!filterDate) return data;
+
+    const formattedFilterDate = filterDate.toISOString().split("T")[0];
+    return data.filter((task) => task.execution_date === formattedFilterDate);
+  }, [data, filterDate]);
 
   const columns = useMemo<ColumnDef<Task>[]>(
     () => [
       {
         accessorKey: "id_task",
-        header: "id",
+        header: "Tarea",
       },
       {
         accessorKey: "task",
-        header: "Tarea",
+        header: "Descripción de la tarea",
         cell: ({ row }) => {
           const taskRow = row.original;
           const isEditing = editingTaskId === taskRow.id_task;
+          const maxLength = 70;
+          const taskText = taskRow.task;
+          const isLong = taskText.length > maxLength;
+          const shortText = isLong
+            ? taskText.slice(0, maxLength) + "..."
+            : taskText;
 
           return isEditing ? (
             <EditableCell
               initialTask={taskRow.task}
               initialStatus={taskRow.status}
+              initialDate={taskRow.execution_date}
               id_task={taskRow.id_task}
               updateTask={updateTask}
               cancelEditing={() => setEditingTaskId(null)}
             />
+          ) : isLong ? (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <span className="cursor-help">{shortText}</span>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <span>{taskText}</span>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           ) : (
             taskRow.task
           );
@@ -146,17 +205,32 @@ export function ListToDo() {
           const isEditing = editingTaskId === taskRow.id_task;
 
           return isEditing ? (
-            <select
-              className="border p-1 rounded w-full text-black"
-              value={editedTaskStatus}
-              onChange={(e) => setEditedTaskStatus(e.target.value)}
-            >
-              <option value="pendiente">Pendiente</option>
-              <option value="completada">Completada</option>
-            </select>
+            <></>
           ) : (
-            taskRow.status
+            <div className="flex items-center gap-2">
+              {taskRow.status === "pendiente" ? (
+                <>
+                  <Clock size={16} className="text-yellow-500" />
+                  {/* <span className="text-yellow-600"></span> */}
+                </>
+              ) : (
+                <>
+                  <CheckCircle size={16} className="text-green-500" />
+                  {/* <span className="text-green-600"></span> */}
+                </>
+              )}
+            </div>
           );
+        },
+      },
+      {
+        accessorKey: "execution_date",
+        header: "Fecha a realizar",
+        cell: ({ row }) => {
+          const fecha = row.original.execution_date;
+          const taskRow = row.original;
+          const isEditing = editingTaskId === taskRow.id_task;
+          return isEditing ? null : fecha;
         },
       },
       {
@@ -180,13 +254,13 @@ export function ListToDo() {
                       setEditedTaskStatus(taskRow.status);
                     }}
                   >
-                    Editar
+                    <Pencil />
                   </Button>
                   <Button
                     variant="destructive"
                     onClick={() => deleteTask(taskRow.id_task)}
                   >
-                    Eliminar
+                    <Trash />
                   </Button>
                 </>
               )}
@@ -198,63 +272,28 @@ export function ListToDo() {
     [editingTaskId, editedTaskText, editedTaskStatus]
   );
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <>
-      <div className="flex gap-2 mb-4">
-        <input
-          type="text"
-          placeholder="Nombre de la tarea"
-          className="border p-2 rounded text-black"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
+      <div className="flex flex-col gap-2 mb-4 ">
+        <CreateToDo
+          task={task}
+          setTask={setTask}
+          status={status}
+          setStatus={setStatus}
+          executionDate={executionDate}
+          setExecutionDate={setExecutionDate}
+          errors={errors}
+          onSave={handleTesRender}
         />
-
-        <select
-          className="border p-2 rounded text-black"
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
-        >
-          <option value="">Selecciona estado</option>
-          <option value="pendiente">Pendiente</option>
-          <option value="completada">Completada</option>
-        </select>
-
-        <Button onClick={handleTesRender}>Guardar tarea</Button>
+        <FilterToDo filterDate={filterDate} setFilterDate={setFilterDate} />
+        <div className=" w-full max-h-[250px] overflow-y-auto border rounded">
+          <TableToDo data={filteredData} columns={columns} />
+        </div>
       </div>
-
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows.map((row) => (
-            <TableRow key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
     </>
   );
 }
